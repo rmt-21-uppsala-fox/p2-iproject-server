2f7902beb1745c669d02da6c7327da09687b2b56
alizta27 <afdaall27@gmail.com>
2022-03-15 08:53:25 +0800

    preparing server

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,14 +1,52 @@
+const { comparePassword, signToken } = require('../helpers/helpers');
+const { School, User } = require('../models');
+
 class Controller {
    static async home(req, res, next) {
       try {
          const allSchools = await School.findAll();
+
+         res.status(200).json({ allSchools });
       } catch (error) {
          next(error);
       }
    }
 
    static async login(req, res, next) {
+      try {
+         const { email, password } = req.body;
 
+         const user = await User.findOne({
+            where: {
+               email,
+               password
+            }
+         });
+
+         if (!user) {
+            throw new Error('Invalid email or password');
+         }
+
+         const passwordCheck = comparePassword(password, user.password);
+
+         if (!passwordCheck) {
+            throw new Error('Invalid email or password');
+         }
+
+         const payload = {
+            id: user.id,
+         }
+
+         const token = signToken(payload);
+
+         res.status(200).json({
+            message: 'User logged in successfully',
+            token
+         });
+
+      } catch (error) {
+         next(error);
+      }
 
    }
 
---
+++ middleware/auth.js
@@ -0,0 +1,34 @@
+const { verifyToken } = require("../helpers/helpers");
+
+const authentification = async (req, res, next) => {
+   try {
+      const { access_token } = req.headers;
+      const checkToken = verifyToken(access_token);
+      if (!checkToken) {
+         throw new Error("Invalid token");
+      }
+
+      let id = payload.id
+      const userLoginData = await User.findByPk(id)
+
+      if (!userLoginData) {
+         throw {
+            name: "Invalid Token"
+         }
+      }
+
+      req.userAccessLogin = {
+         id: userLoginData.id,
+         email: userLoginData.email,
+         firstName: userLoginData.firstName,
+         lastName: userLoginData.lastName,
+         schoolId: userLoginData.schoolId,
+         role: userLoginData.role
+      }
+
+      next();
+
+   } catch (error) {
+      next(error);
+   }
+}
