ec1b333ad3c918710d104fdec90b7df71289b47a
imramzani <ramzani.imam@gmail.com>
2022-03-21 23:05:38 +0700

    initial setup for user and api check

---
+++ app.js
@@ -0,0 +1,46 @@
+if (process.env.NODE_ENV == 'production') {
+  require('dotenv').config()
+}
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 3000
+const axios = require("axios")
+// const { authn } = require('./middleware/auth')
+// const foodRoutes = require('./router/food')
+// const historyRoutes = require('./router/history')
+// const customerRoutes = require('./router/customer')
+// const userController = require('./controller/userController')
+// const foodController = require('./controller/foodController')
+// const categoryController = require('./controller/categoryController')
+// const customerController = require('./controller/customerController')
+// const errorHandler = require('./middleware/errorHandling')
+
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get("/")
+
+// app.get('/category', categoryController.getCategory)
+// app.post('/register', userController.register)
+// app.post('/login', userController.login)
+// app.post('/authGoogle', userController.authGoogle)
+// app.post('/pub/register', customerController.register)
+// app.post('/pub/login', customerController.login)
+// app.post('/pub/authGoogle', customerController.authGoogle)
+// app.get('/pub/food', customerController.getConditionalFood)
+// app.get('/pub/food/:foodId', customerController.getFoodById)
+
+// app.use(authn)
+// app.use('/food', foodRoutes)
+// app.use('/history', historyRoutes)
+// app.use('/pub/myfav', customerRoutes)
+
+// app.use(errorHandler)
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`)
+})
+
+module.exports = app
---
+++ models/user.js
@@ -0,0 +1,60 @@
+'use strict';
+const bcrypt = require("bcrypt")
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Name is required" },
+        notNull: { msg: "Name is required" },
+      }
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Image Url is required" },
+        notNull: { msg: "Image Url is required" },
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: { msg: "Email must be unique" },
+      validate: {
+        notEmpty: { msg: "Email is required" },
+        notNull: { msg: "Email is required" },
+        isEmail: { msg: "Invalid email format" }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Password is required" },
+        notNull: { msg: "Password is required" },
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  User.beforeCreate((instance) => {
+    instance.password = bcrypt.hashSync(instance.password, 8)
+  })
+  return User;
+};
