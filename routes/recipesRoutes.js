dee9cbc940523ddac4f8638265634535267d676f
rosaalwyh <alawiyahrosa@gmail.com>
2022-03-22 21:26:23 +0700

    done read

--- routes/recipesRoutes.js
+++ routes/recipesRoutes.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const RecipeController = require('../controllers/RecipeController')
 
 router.get('/recipes', RecipeController.getRecipes)
+router.get('/recipes/filter', RecipeController.getFilteredRecipes)
+router.post('/recipes/:RecipeId', RecipeController.createBookmark)
 
 module.exports = router
