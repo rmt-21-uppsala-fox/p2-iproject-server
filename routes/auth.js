affe7dcbdf5bd39e36cf8f46716d577e48c5e47e
rosaalwyh <alawiyahrosa@gmail.com>
2022-03-22 22:10:53 +0700

    done login

--- routes/auth.js
+++ routes/auth.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const AuthController = require('../controllers/AuthController')
 
 router.post('/register', AuthController.register)
+router.post('/login', AuthController.login)
 
 module.exports = router
