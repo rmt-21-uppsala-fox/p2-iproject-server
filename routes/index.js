5b993d88b39ead370256c8d3dd4c66c2ea3cf071
riorichard98 <riorichard12@gmail.com>
2022-03-22 22:29:46 +0700

    bit tidyup code on development

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,11 @@ const express = require('express')
 const router = express.Router()
 const postRoute = require('./posts.js')
 const userRoute = require('./users.js')
-
 const relationRoute = require('./relations.js')
+
 router.use('/posts',postRoute)
 router.use('/users',userRoute)
-
 router.use('/relations',relationRoute)
-
-
 
 
 module.exports = router
