cff92b3b203e4d79bd884125ee5c64b13a3adf35
Muhammad Naufal Hilmi <muhammadnhilmi@gmail.com>
2022-03-22 23:42:12 +0700

    UpcomingGame done

--- controllers/controllerGame.js
+++ controllers/controllerGame.js
@@ -1,11 +1,11 @@
 const keyOfRAWG = process.env.key;
 const axios = require(`axios`);
 const date = new Date();
+const dateNow = date.toISOString().slice(0, 10);
 
 class Controller {
   static async showGames(req, res, next) {
     try {
-      const dateNow = date.toISOString().slice(0, 10);
       date.setDate(date.getDate() - 0);
       date.setFullYear(date.getFullYear() - 1);
       const aYearBefore = date.toISOString().slice(0, 10);
@@ -24,7 +24,26 @@ class Controller {
       next(err);
     }
   }
-  
+  static async UpcomingNextWeek(req, res, next) {
+    try {
+      date.setDate(date.getDate() + 7);
+      date.setFullYear(date.getFullYear() - 0);
+      const nextWeek = date.toISOString().slice(0, 10);
+      // console.log(test);
+      const data = await axios({
+        method: `get`,
+        url: `https://api.rawg.io/api/games`,
+        params: {
+          key: `7b6f7730d9af4cfebf2a880376bda74c`,
+          dates: `${dateNow},${nextWeek}`,
+          ordering: `-added`,
+        },
+      });
+      res.status(200).json(data.data.results);
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
 module.exports = Controller;
