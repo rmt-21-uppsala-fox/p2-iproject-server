2ebd61cd3a8ce37b3af3206734d6c211fc2af4d5
mwbintang <94796835+mwbintang@users.noreply.github.com>
2022-03-22 14:17:54 +0700

    setup and migrate table

---
+++ migrations/20220322071052-create-playlist.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Playlists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references:{
+          model:"Users",
+          key:"id"
+        },
+        onUpdate:"cascade",
+        onDelete:"cascade"
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      genre: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      artist: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Playlists');
+  }
+};
