6fd520424b120cef5428289b4c56c35e6f57fa62
Rizki Arifudin <rizkiarifudin45@gmail.com>
2022-03-22 13:08:17 +0700

    Initializing project, Sequelize, database, ERD

---
+++ migrations/20220322043024-create-comment.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      PostId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Posts',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      comment: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Comments');
+  }
+};
---
+++ models/post.js
@@ -0,0 +1,32 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Post.belongsTo(models.User, { foreignKey: "UserId" }),
+        Post.belongsTo(models.User, { foreignKey: "totalLike" }),
+        Post.belongsTo(models.Category, { foreignKey: "categoryId" });
+        Post.hasMany(models.User, {through: models.Comment, foreignKey: "PostId"})
+
+    }
+  }
+  Post.init(
+    {
+      imgUrl: DataTypes.STRING,
+      caption: DataTypes.STRING,
+      UserId: DataTypes.INTEGER,
+      totalLike: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Post",
+    }
+  );
+  return Post;
+};
