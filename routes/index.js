04ed09467621ad21952a52436e5c90f372275589
AndrizalChaidar <andrizal.cs@gmail.com>
2022-03-23 02:46:33 +0700

    revision setup-init only walletcards route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const walletCardRoute = require("./walletcards");
 const router = express.Router();
 
-router.get("/");
 router.use("/walletcards", walletCardRoute);
 
 module.exports = router;
