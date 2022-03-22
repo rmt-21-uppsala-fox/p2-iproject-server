f805be13ac6cf87271b021ca8af67e79fb09a3eb
riorichard98 <riorichard12@gmail.com>
2022-03-22 22:04:52 +0700

    user login done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const postRoute = require('./posts.js')
+const userRoute = require('./users.js')
 
 router.use('/posts',postRoute)
+router.use('/users',userRoute)
 
 module.exports = router
