8c0145a2afcd585340abcf5bd671a77d88795564
Muhammad Naufal Hilmi <muhammadnhilmi@gmail.com>
2022-03-22 20:20:12 +0700

    login done

--- controllers/controllerUser.js
+++ controllers/controllerUser.js
@@ -1,4 +1,6 @@
 const { User } = require(`../models/index.js`);
+const { comparePass } = require(`../helpers/bcrypt.js`);
+const { createToken } = require(`../helpers/jwt.js`);
 class controller {
   static async regist(req, res, next) {
     try {
@@ -11,6 +13,54 @@ class controller {
       });
       res.status(201).json({ msg: `Welcome ${data.username}` });
     } catch (err) {
+      next(err);
+    }
+  }
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      if (!email) {
+        throw {
+          name: `Email is required`,
+          message: `Email is required`,
+        };
+      } else if (!password) {
+        throw {
+          name: `Password is required`,
+          message: `Password is required`,
+        };
+      }
+
+      const data = await User.findOne({ where: { email } });
+
+      if (!data) {
+        throw {
+          name: `Invalid Email/Password`,
+          message: `Invalid Email/Password`,
+        };
+      }
+
+      const passUser = comparePass(password, data.password);
+      // console.log(passUser);
+      
+      if (!passUser) {
+        throw {
+          name: `Invalid Email/Password`,
+          message: `Invalid Email/Password`,
+        };
+      }
+
+      const payload = { id: data.id };
+
+      // console.log(payload);
+
+      const token = createToken(payload);
+
+      // console.log(token);
+
+      res.status(200).json({ access_token: token });
+    } catch (err) {
+      // console.log(err);
       next(err);
     }
   }
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require(`../controllers/controllerUser`);
 
 router.post(`/register`, controller.regist);
+router.post(`/login`, controller.login);
 
 module.exports = router;
