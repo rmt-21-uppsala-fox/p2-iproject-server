009fbf11a495b643abc95c55e9c69f333d657e99
Busyairi AR <busyairiar@gmail.com>
2022-03-23 00:56:50 +0700

    checkpoint create order & payment xendit

--- routes/order.js
+++ routes/order.js
@@ -3,6 +3,8 @@ const OrderController = require('../controllers/OrderController');
 
 
 router.get('/orders', OrderController.getAllOrder)
-router.post('/neworder', OrderController.createNewOrder)
+router.post('/neworder', OrderController.newOrder)
+router.post('/payment', OrderController.orderPayment)
+router.patch('/receipt', OrderController.receipt)
 
 module.exports = router
