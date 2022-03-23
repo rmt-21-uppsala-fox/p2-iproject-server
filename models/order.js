54e787b1ddd3399d06f45b9dff01416c77a42219
brryar <brryarch@gmail.com>
2022-03-23 19:57:46 +0700

    fix association

--- models/order.js
+++ models/order.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Order.hasMany(models.Menu)
+      Order.belongsTo(models.Menu)
     }
   }
   Order.init({
