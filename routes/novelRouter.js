5295af1d3fc025825a36500da4dd5794594d79a8
dzulfiqarzaky <dzulfiqarzakyaulia@gmail.com>
2022-03-23 07:16:26 +0700

    add new router for novel/:genre

--- routes/novelRouter.js
+++ routes/novelRouter.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const NovelController = require('../controllers/NovelController');
 const router = express.Router();
 
 router.get('/novel', (req, res) => {
     res.redirect('/novel/:genre');
 });
+
+router.get('/novel/:genre', NovelController.NovelListAndGenre);
+
 module.exports = router;
