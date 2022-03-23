0b26e65420ec6021d49771677b70cff6f4fe0884
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-03-23 18:47:37 +0700

    setup-nodemailer done

--- server/controllers/donationController.js
+++ server/controllers/donationController.js
@@ -1,4 +1,5 @@
-const { UserHistory, Donation } = require("../models/index");
+const { User, UserHistory, Donation } = require("../models/index");
+const nodemailer = require("nodemailer");
 
 class DonationController {
   static async getAllDonation(req, res, next) {
@@ -51,6 +52,7 @@ class DonationController {
         { status: "success" },
         { where: { id: UserHistoryId }, returning: true }
       );
+      let findemailUser = await User.findByPk(findData.UserId);
       let findDonation = await Donation.findByPk(findData.DonationId);
       await Donation.update(
         {
@@ -58,6 +60,30 @@ class DonationController {
         },
         { where: { id: findData.DonationId } }
       );
+      let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "kitabantuuu@gmail.com",
+          pass: "renesdwir7",
+        },
+      });
+
+      let mailOptions = {
+        from: "platform.kitabantu@gmail.com",
+        to: `${findemailUser.email}`,
+        subject: "Donation Success",
+        // text: `Terima kasih atas donasi anda, semoga anda diberikan rezeki yang berlimpah!, ${findemailUser.email}.`,
+        text: `<p style="text-color:blue;"> Terima kasih atas donasi anda, semoga anda diberikan rezeki yang berlimpah! </p>`,
+      };
+
+      transporter.sendMail(mailOptions, function (err, succes) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Email is sent");
+        }
+      });
+
       res.status(200).json({ message: "success payment" });
     } catch (error) {
       next(error);
