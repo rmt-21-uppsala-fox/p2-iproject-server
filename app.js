2ad1d876d945d29c8035a5c0c6057b31283670be
mwbintang <94796835+mwbintang@users.noreply.github.com>
2022-03-24 01:40:24 +0700

    nodemailer

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
 const port = 3000
+const routerUser = require('./router/userRoute')
+const cors = require('cors')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/', routerUser)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
