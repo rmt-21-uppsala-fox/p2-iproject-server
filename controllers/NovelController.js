fd8dd4967e6fef94d0501d184204e84f07ddfbd3
dzulfiqarzaky <dzulfiqarzakyaulia@gmail.com>
2022-03-23 07:23:05 +0700

    add new class for router components

--- controllers/NovelController.js
+++ controllers/NovelController.js
@@ -8,6 +8,25 @@ class NovelController {
             next(err);
         }
     }
+
+    static async NovelChapter(req, res, next) {
+        try {
+            const title = req.params.title;
+            const chapter = req.params.chapter;
+            res.status(200).json('hello');
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    static async NovelDetail(req, res, next) {
+        try {
+            const title = req.params.title;
+            res.status(200).json('uwo');
+        } catch (err) {
+            next(err);
+        }
+    }
 }
 
 module.exports = NovelController;
