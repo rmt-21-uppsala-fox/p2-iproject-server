3743e4a59b91c739bf4d0f5b044cf9715d4be371
mwbintang <94796835+mwbintang@users.noreply.github.com>
2022-03-24 10:30:02 +0700

    debug heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = proccess.env.PORT
 const routerUser = require('./router/userRoute')
 const cors = require('cors')
 const errorHandler = require('./middleware/errorHandler')
