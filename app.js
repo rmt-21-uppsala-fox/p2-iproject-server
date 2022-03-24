4c41000076beec69f4ea949754db1a88c66c4bdc
Dioauliaharrisa <daharrisa@gmail.com>
2022-03-24 08:40:01 +0700

    API docs:created

--- app.js
+++ app.js
@@ -36,10 +36,6 @@ app.post("/paintings", async (request, response) => {
 });
 app.post("/wikis", async (request, response) => {
   try {
-    // console.log(request.body);
-    let { title } = request.body;
-    console.log(22, title);
-
     const wikisData0 = await axios.get(
       `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&prop=info&inprop=url&search=van_gogh_self_portrait`
     );
@@ -52,24 +48,24 @@ app.post("/wikis", async (request, response) => {
     const videoData0 = await axios.get(
       `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_KEY}&type=video&q=gogh_self_portrait_analysis`
     );
-    const videoData1 = await axios.get(
-      `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_KEY}&type=video&q=night_watch_analysis`
-    );
-    const videoData2 = await axios.get(
-      `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_KEY}&type=video&q=fishing_for_souls_painting`
-    );
+    // const videoData1 = await axios.get(
+    //   `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_KEY}&type=video&q=night_watch_analysis`
+    // );
+    // const videoData2 = await axios.get(
+    //   `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_KEY}&type=video&q=fishing_for_souls_painting`
+    // );
+    // console.log(videoData0, videoData1, videoData2);
 
     response.status(200).json({
       data0: wikisData0.data[3][0],
       data1: wikisData1.data[3][0],
       data2: wikisData2.data[3][0],
-      videoData0: `https://www.youtube.com/watch?v=${videoData0.data.items[0].id.videoId}`,
-      videoData1: `https://www.youtube.com/watch?v=${videoData1.data.items[0].id.videoId}`,
-      videoData2: `https://www.youtube.com/watch?v=${videoData2.data.items[0].id.videoId}`
-
+      // videoData0: `https://www.youtube.com/watch?v=${videoData0.data.items[0].id.videoId}`,
+      // videoData1: `https://www.youtube.com/watch?v=${videoData1.data.items[0].id.videoId}`,
+      // videoData2: `https://www.youtube.com/watch?v=${videoData2.data.items[0].id.videoId}`,
     });
   } catch (error) {
-    console.log(error);
+    console.log(error.response);
     response.status(500).json({ message: "Internal server error" });
   }
 });
