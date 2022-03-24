b5baef68a60ff81a59c004144fff974c3125c73e
rosaalwyh <alawiyahrosa@gmail.com>
2022-03-24 10:47:12 +0700

    reset port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const express = require('express')
 //     Server
 // } = require("socket.io");
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000;
 const recipeRoutes = require('./routes/recipes')
 const authRoutes = require('./routes/auth');
 app.use(cors());
