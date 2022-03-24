34ae19f65ef9547627edf6eb1109480e5f84a50c
Muhammad Ihsan Erdiansyah <Ihsan.erdi97@gmail.com>
2022-03-24 11:35:31 +0700

    setup deploy

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const menuRoute = require("./routes/menuRoute");
 const customerRoute = require("./routes/customerRoute");
 const orderRoute = require("./routes/orderRoute");
 const xenditController = require("./controllers/controllerXendit");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+
 const jwt = require("jsonwebtoken");
 const secretKey = process.env.Secret_key;
 
