dcd4535d0184b4d0b5b08e51af9296d2b35484d1
sammyon <samamandre.mandracon@gmail.com>
2022-03-24 10:59:34 +0700

    firestore

--- app.js
+++ app.js
@@ -6,17 +6,68 @@ const express = require("express");
 const errorHandler = require("./helpers/errorHandler");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const cors = require('cors')
-const routes = require('./routes')
+const cors = require("cors");
+const routes = require("./routes");
+const { createServer } = require("http");
+const { Server } = require(`socket.io`);
+// const httpServer = createServer(app);
 
-app.use(cors())
+
+
+// httpServer.listen(PORT, () => {
+//   console.log(`Socket Listening to PORT ${PORT}`);
+// })
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", routes)
+app.use("/", routes);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Listening to PORT ${PORT}`);
-});
+const server = app.use(cors()).listen(PORT, () =>
+console.log(`Listening to PORT ${PORT}`))
+
+const io = new Server(server, {
+  cors: {
+    origin: `*`
+  }
+})
+
+let chatUsers = []
+let chatHistory = []
+
+io.on('connection', (socket) => {
+  // console.log(socket);
+
+  //? disconnected
+  socket.on('disconnect', (reason) => {
+    console.log(`Disconnected`, reason);
+  })
+
+  //? custom events
+  socket.on('clientRequest', (payload) => {
+    console.log(`Custom event listener 1`, payload);
+
+    //! emit ke client
+    socket.emit('serverResponse', "serverResponse")
+  })
+
+  socket.on('setUser', (payload) => {
+    chatUsers.push({
+      email: payload.email,
+      status: 'online'
+    })
+  })
+
+  socket.on('clientMessage', (payload) => {
+    // console.log(`MASUK CHAT`, payload.message);
+    chatHistory.push(payload.message)
+    io.emit("messagesHistory", chatHistory)
+  })
+})
+
+// app.listen(PORT, () => {
+//   console.log(`Listening to PORT ${PORT}`);
+// });
--- controllers/users.js
+++ controllers/users.js
@@ -5,9 +5,12 @@ const clientID = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 // Import the functions you need from the SDKs you need
 const { initializeApp } = require ("firebase/app");
 const { getFirestore, doc, collection, addDoc, getDoc } = require ('firebase/firestore');
+// const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+// const { getFirestore, doc, collection, addDoc, getDoc  } = require('firebase-admin/firestore');
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -37,6 +40,10 @@ class ControllerUsers {
       })
       const checkUser = await getDoc(doc(firestore, `Users/${response._key.path.segments[1]}`))
       // console.log(checkUser);
+      // nama collection
+      // Users:index
+
+      // snapshosts 2 kali
       if (checkUser.exists()) {
         const newUser = checkUser.data()
         // console.log(newUser, `MASUK`);
@@ -45,7 +52,7 @@ class ControllerUsers {
         throw {
           throw: true,
           status: 500,
-          message: 'internal server error'
+          message: 'Fail Firestore'
         }
       }
     } catch (error) {
@@ -53,48 +60,15 @@ class ControllerUsers {
     }
   }
 
-  static async loginGoogle(req, res, next) {
+  static login(req, res, next) {
     try {
-      const { googleToken } = req.body;
-      const ticket = await clientID.verifyIdToken({
-        idToken: googleToken,
-        audience: process.env.CLIENT_ID,
-      });
-
-      const payload = ticket.getPayload();
-
-      let [user, isCreated] = await User.findOrCreate({
-        where: {
-          email: payload.email,
-        },
-        defaults: {
-          email: payload.email,
-          password: payload.email,
-          role: "user",
-        },
-      });
-
-      if (!isCreated && user.address) {
-        const payload = {
-          userId: user.id,
-          email: user.email,
-          role: user.role,
-        };
-
-        const token = signToken(payload);
-
-        res
-          .status(201)
-          .json({ email: user.email, isCreated, access_token: token });
-      } else {
-        res.status(201).json({ email: user.email, isCreated });
-      }
+      
     } catch (error) {
-      next(error);
+      
     }
   }
 
-  static async addBookmark(req, res, next) {
+  static async addFavorite(req, res, next) {
     try {
       const { UserId } = req.user;
       const { AnimeId } = req.body;
@@ -108,79 +82,6 @@ class ControllerUsers {
         AnimeId: 1
       })
       res.status(201).json(response);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  static async deleteBookmark(req, res, next) {
-    try {
-      const { bookmarkId } = req.params;
-
-      await Bookmark.destroy({
-        where: {
-          id: bookmarkId,
-        },
-      });
-      res.status(201).json({ message: `Removed from Watch List` });
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  static async getBookmark(req, res, next) {
-    try {
-      const { role, UserId } = req.user;
-
-      const bookmarks = await Bookmark.findAll({
-        include: {
-          model: Job,
-          include: {
-            model: Company,
-          },
-        },
-        where: {
-          UserId,
-        },
-      });
-
-      res.status(200).json(bookmarks);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  static async getJobs(req, res, next) {
-    try {
-      const { jobType, company, page } = req.query;
-      // console.log(jobType, company, page, `page`);
-      const search = {
-        include: {
-          model: Company,
-        },
-        where: {
-          status: {
-            [Op.not]: "archived",
-          },
-        },
-        limit: 8,
-        order: [["id", "ASC"]],
-      };
-      if (jobType) {
-        search.where.jobType = jobType;
-      }
-      if (company) {
-        search.where.companyId = company;
-      }
-      if (page) {
-        search.offset = 8 * (page - 1);
-      }
-      // console.log(search , `search`);
-      let jobs = await Job.findAndCountAll(search);
-      let pages = Math.ceil(jobs.count / 8);
-      // console.log(jobs);
-      // console.log(pages, jobs.count);
-      res.status(200).json({ count: pages, jobs: jobs.rows });
     } catch (error) {
       next(error);
     }
