d204ab25eab2f1aaf7391f6e95964b5a60ee4cb0
sammyon <samamandre.mandracon@gmail.com>
2022-03-25 09:39:30 +0700

    fix documentation and delete logs

--- middlewares/authc.js
+++ middlewares/authc.js
@@ -4,13 +4,14 @@ const { Users } = require("../controllers/users");
 let authc = async (req, res, next) => {
   try {
     const { access_token } = req.headers;
+    // console.log(`masuk AUTH`, access_token);
     const payload = verifyToken(access_token);
-    console.log(payload, `MASUK ATUH`);
+    // console.log(payload, `MASUK ATUH`);
 
     const userSnapshot = await Users.doc(payload.id).get()
     const user = userSnapshot.data()
     // console.log(payload, userSnapshot.id, user, `AUTHC DATA`);
-
+    
     if (!user) {
       throw {
         name: `JsonWebTokenError`,
